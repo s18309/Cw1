b8c24b27bad5e4a70cadbfe169b8fd8b4322feb3
Urszula Tarasewicz <s18309@pjwstk.edu.pl>
2020-03-08 22:20:54 +0100

    dodano bledy

--- Cw1/Person.cs
+++ Cw1/Person.cs
@@ -9,24 +9,39 @@ namespace Cw1
     {
         public static async Task Main(string[] args)
         {
+            if (args.Length < 2)
+                throw new ArgumentNullException();
+
+
             var url = "https://www.pja.edu.pl";
-            var httpCLient =  new HttpClient();
+            var httpCLient = new HttpClient();
 
             var response = await httpCLient.GetAsync(url);
 
             if (response.IsSuccessStatusCode)
             {
+
                 var htmlContent = await response.Content.ReadAsStringAsync();
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+" ,RegexOptions.IgnoreCase);
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
                 var matches = regex.Matches(htmlContent);
+                int bylo = 0;
+
                 foreach (var match in matches)
                 {
                     Console.WriteLine(match.ToString());
+                    bylo++;
                 }
+
+                if (bylo == 0)
+                    throw new Exception("Nie znaleziono adresow e-mail");
+
+
             }
+            else
+                throw new Exception("blad w czasie pobierania strony");
+
+            httpCLient.Dispose();
 
         }
     }
 }
-
-
